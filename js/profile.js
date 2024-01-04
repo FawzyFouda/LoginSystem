73756d3448b9912b9a34bc5f61c007fa72bd8ceb
FawzyFouda <fawzy.fooda00@gmail.com>
2024-01-04 19:41:56 +0200

    .

--- js/profile.js
+++ js/profile.js
@@ -2,12 +2,12 @@
  function logout(){                                 // after log out remove name and email from local storage
     localStorage.removeItem('name')
     localStorage.removeItem('email')
-    window.location.href = `login.html`
+    window.location.href = `index.html`
 }
 
 // -----if user try to back to profile after log out ,he can not access to profile befor log in again--------------------------------------
 document.getElementById('user-profile').innerHTML = `${localStorage.getItem('name')} profile`
 let name = localStorage.getItem('name')?localStorage.getItem('name'):''
 if(name ==''){
-window.location.href = `login.html`
+window.location.href = `index.html`
 }
